c06f86b53dac9d30489a192b7f7f1eef7b095e94
Kirill Klimov <ra22341@ya.ru>
2019-04-12 15:29:48 +0300

    increase version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="geom_tools",
-    version="0.0.1",
+    version="0.0.2",
     author="Daiver",
     author_email="ra22341@ya.ru",
     description="",
