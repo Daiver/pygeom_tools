37b9e30d80aa1e16153b32fca4c8eb5a80b45ca3
Kirill Klimov <ra22341@ya.ru>
2019-07-23 15:26:07 +0300

    fixed missed import

--- geom_tools/__init__.py
+++ geom_tools/__init__.py
@@ -1,4 +1,4 @@
 from . import utils
 from .mesh import Mesh
-from .obj_import import from_obj_string, from_obj_file
+from .obj_import import load, from_obj_string, from_obj_file
 from .obj_export import save, save_to_stream, export_vertices_by_existing_obj, export_vertices_by_existing_obj_stream
