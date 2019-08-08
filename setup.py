fdb9f968b16908419e817cf90309433c4a7f1345
Kirill Klimov <ra22341@ya.ru>
2019-08-08 12:29:20 +0300

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="geom_tools",
-    version="0.1.0",
+    version="0.1.1",
     author="Daiver",
     author_email="ra22341@ya.ru",
     description="",
