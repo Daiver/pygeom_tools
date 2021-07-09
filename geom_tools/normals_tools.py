f6847eec624bdd0362375db78513dbe9a200da0e
Kirill Klimov <ra22341@ya.ru>
2021-07-09 10:44:14 +0300

    allow zero length normals

--- geom_tools/normals_tools.py
+++ geom_tools/normals_tools.py
@@ -1,7 +1,11 @@
 import numpy as np
 
 
-def compute_normals_for_triangles(vertices: np.ndarray, triangle_vertex_indices: np.ndarray) -> np.ndarray:
+def compute_normals_for_triangles(
+        vertices: np.ndarray,
+        triangle_vertex_indices: np.ndarray,
+        eps_zero_length: float = 1e-8
+) -> np.ndarray:
     """
     Assumes anti-clockwise vertices order
     """
@@ -13,11 +17,15 @@ def compute_normals_for_triangles(vertices: np.ndarray, triangle_vertex_indices:
     edges2 = vertices_for_triangles[:, 2] - vertices_for_triangles[:, 0]
     cross = np.cross(edges1, edges2)
     length = np.linalg.norm(cross, axis=1).reshape(-1, 1)
-    # TODO: check zero length
+    length = np.where(length > eps_zero_length, length, 1)
     return cross / length
 
 
-def compute_vertices_normals_from_triangles(vertices: np.ndarray, triangle_vertex_indices: np.ndarray) -> np.ndarray:
+def compute_vertices_normals_from_triangles(
+        vertices: np.ndarray,
+        triangle_vertex_indices: np.ndarray,
+        eps_zero_length=1e-8
+) -> np.ndarray:
     """
     Assumes anti-clockwise vertices order
     Super slow can be written in C++
@@ -26,12 +34,12 @@ def compute_vertices_normals_from_triangles(vertices: np.ndarray, triangle_verte
     assert triangle_vertex_indices.ndim == 2 and triangle_vertex_indices.shape[1] == 3
     n_vertices = vertices.shape[0]
     n_triangles = triangle_vertex_indices.shape[0]
-    normals_per_triangle = compute_normals_for_triangles(vertices, triangle_vertex_indices)
+    normals_per_triangle = compute_normals_for_triangles(vertices, triangle_vertex_indices, eps_zero_length)
     res = np.zeros((n_vertices, 3), dtype=np.float32)
     for tri_ind in range(n_triangles):
         for v_ind in triangle_vertex_indices[tri_ind]:
             res[v_ind] += normals_per_triangle[tri_ind]
 
     length = np.linalg.norm(res, axis=1).reshape(-1, 1)
-    # TODO: check zero length
+    length = np.where(length > eps_zero_length, length, 1)
     return res / length
--- tests/test_normals_tools.py
+++ tests/test_normals_tools.py
@@ -124,6 +124,23 @@ class TestNormalsTools(unittest.TestCase):
         self.assertEqual(ans.shape, res.shape)
         self.assertTrue(np.allclose(ans, res))
 
+    def test_compute_normals_for_triangles07(self):
+        vertices = np.array([
+            [0, 0, 0],
+            [1, 0, 0],
+            [1, 0, 0],
+        ], dtype=np.float32)
+        triangle_vertex_indices = np.array([
+            [0, 1, 2]
+        ], dtype=np.int32)
+        ans = np.array([
+            [0, 0, 0]
+        ], dtype=np.float32)
+
+        res = normals_tools.compute_normals_for_triangles(vertices, triangle_vertex_indices)
+        self.assertEqual(ans.shape, res.shape)
+        self.assertTrue(np.allclose(ans, res))
+
     def test_compute_vertices_normals_from_triangles01(self):
         vertices = np.array([
             [0, 0, 0],
@@ -208,6 +225,24 @@ class TestNormalsTools(unittest.TestCase):
             [0, -1, 0],
             [0, -1, 0],
             [0, -0.70710678, -0.70710678],
+        ], dtype=np.float32)
+        res = normals_tools.compute_vertices_normals_from_triangles(vertices, triangle_vertex_indices)
+        self.assertEqual(ans.shape, res.shape)
+        self.assertTrue(np.allclose(ans, res))
+
+    def test_compute_vertices_normals_from_triangles05(self):
+        vertices = np.array([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 1, 0],
+        ], dtype=np.float32)
+        triangle_vertex_indices = np.array([
+            [0, 1, 2]
+        ], dtype=np.int32)
+        ans = np.array([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
         ], dtype=np.float32)
         res = normals_tools.compute_vertices_normals_from_triangles(vertices, triangle_vertex_indices)
         self.assertEqual(ans.shape, res.shape)
