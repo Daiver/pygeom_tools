368a5fce2df46a83d074c71792017263c0106507
Kirill Klimov <ra22341@ya.ru>
2021-07-26 14:52:43 +0300

    geom_tools.rigid_alignment.find_rotation_and_translation_weighted added and tested

--- geom_tools/rigid_alignment.py
+++ geom_tools/rigid_alignment.py
@@ -1,7 +1,7 @@
 from typing import Tuple
 import numpy as np
 
-from .utils import center_of_vertices
+from .utils import center_of_vertices, center_of_vertices_weighted
 from .transformations import translated
 
 
@@ -20,8 +20,35 @@ def find_rotation_and_translation(src: np.ndarray, dst: np.ndarray) -> Tuple[np.
     return rotation, translation
 
 
+def find_rotation_and_translation_weighted(
+        src: np.ndarray,
+        dst: np.ndarray,
+        weights: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray]:
+    assert src.ndim == 2
+    assert src.shape == dst.shape
+    assert weights.ndim == 1 and weights.shape[0] == src.shape[0]
+    src_center = center_of_vertices_weighted(src, weights)
+    dst_center = center_of_vertices_weighted(dst, weights)
+
+    src_centered = translated(src_center, src)
+    dst_centered = translated(dst_center, dst)
+    cov_mat = cov_mat_from_vertices_weighted(src_centered, dst_centered, weights)
+
+    rotation = rot_mat_from_cov_mat(cov_mat)
+    translation = dst_center - rotation @ src_center
+    return rotation, translation
+
+
 def cov_mat_from_vertices(src: np.ndarray, dst: np.ndarray) -> np.ndarray:
     return src.T @ dst
+
+
+def cov_mat_from_vertices_weighted(src: np.ndarray, dst: np.ndarray, weights_vec: np.ndarray) -> np.ndarray:
+    assert weights_vec.ndim == 1
+    assert weights_vec.shape[0] == src.shape[0]
+    tmp = src * weights_vec[:, None]
+    return tmp.T @ dst
 
 
 def rot_mat_from_cov_mat(cov_mat: np.ndarray) -> np.ndarray:
--- tests/test_rigid_alignment.py
+++ tests/test_rigid_alignment.py
@@ -74,6 +74,100 @@ class TestRigidAlignment(unittest.TestCase):
         res = geom_tools.transform_vertices(transformation, src)
         self.assertTrue(geom_tools.utils.is_arrays_equal(ans, res))
 
+    def test_cov_mat_from_vertices_weighted01(self):
+        vertices1 = np.array([
+            [1, 2, 0],
+        ], dtype=np.float32)
+        vertices2 = np.array([
+            [3, 5, 0],
+        ], dtype=np.float32)
+        weights = np.array([
+            2
+        ])
+        ans = np.array([
+            [6, 10, 0],
+            [12, 20, 0],
+            [0, 0, 0],
+        ], dtype=np.float32)
+        res = geom_tools.rigid_alignment.cov_mat_from_vertices_weighted(vertices1, vertices2, weights)
+        self.assertTrue(geom_tools.utils.is_arrays_equal(ans, res))
+
+    def test_find_rotation_and_translation_weighted01(self):
+        vertices1 = np.array([
+            [-1, 0, 0],
+            [0, -1, 0],
+            [1, 0, 0],
+            [0, 1, 0]
+        ])
+        vertices2 = np.array([
+            [-1, 0, 0],
+            [0, -1, 0],
+            [1, 0, 0],
+            [0, 1, 0]
+        ])
+        weights = np.array([1, 1, 1, 1])
+        ans_trans = np.array([0, 0, 0], dtype=np.float32)
+        ans_rot = np.eye(3)
+        res_rot, res_trans = geom_tools.rigid_alignment.find_rotation_and_translation_weighted(
+            vertices1, vertices2, weights
+        )
+        self.assertTrue(geom_tools.utils.is_arrays_equal(ans_rot, res_rot))
+        self.assertTrue(geom_tools.utils.is_arrays_equal(ans_trans, res_trans))
+
+    def test_find_rotation_and_translation_weighted02(self):
+        vertices1 = np.array([
+            [-1, 0, 0],
+            [0, -1, 0],
+            [1, 0, 0],
+            [1, 20, -55],
+            [0, 1, 0]
+        ])
+        vertices2 = np.array([
+            [-1, 0, 0],
+            [0, -1, 0],
+            [1, 0, 0],
+            [100, 2340, 55],
+            [0, 1, 0]
+        ])
+        weights = np.array([1, 1, 1, 0, 1])
+        ans_trans = np.array([0, 0, 0], dtype=np.float32)
+        ans_rot = np.eye(3)
+        res_rot, res_trans = geom_tools.rigid_alignment.find_rotation_and_translation_weighted(
+            vertices1, vertices2, weights
+        )
+        self.assertTrue(geom_tools.utils.is_arrays_equal(ans_rot, res_rot))
+        self.assertTrue(geom_tools.utils.is_arrays_equal(ans_trans, res_trans))
+
+    def test_find_rotation_and_translation_weighted03(self):
+        vertices1 = np.array([
+            [-1, 0, 0.5],
+            [-2, -4, 5],
+            [0, -1, 0.5],
+            [1, 0, 0.5],
+            [0, 1, 0.5],
+            [22, -5, 2],
+        ])
+        vertices2 = np.array([
+            [0, 2, 1],
+            [13, 3, 3],
+            [-2, 0, 1],
+            [0, -2, 1],
+            [2, 0, 1],
+            [16, 55, 33],
+        ])
+        weights = np.array([1, 0, 1, 1, 1, 0])
+        ans_trans = np.array([0, 0, 0.5], dtype=np.float32)
+        ans_rot = np.array([
+            [0, 1, 0],
+            [-1, 0, 0],
+            [0, 0, 1]
+        ], dtype=np.float)
+        res_rot, res_trans = geom_tools.rigid_alignment.find_rotation_and_translation_weighted(
+            vertices1, vertices2, weights
+        )
+        self.assertTrue(geom_tools.utils.is_arrays_equal(ans_rot, res_rot))
+        self.assertTrue(geom_tools.utils.is_arrays_equal(ans_trans, res_trans))
+
 
 if __name__ == '__main__':
     unittest.main()
